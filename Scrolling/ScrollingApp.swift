00ca110e77135fa7932b0193b651962166ebd365
Datafox-JCP <datafox@outlook.com>
2024-02-11 06:22:44 -0600

    Initial Commit

---
+++ Scrolling/ScrollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollingApp.swift
+//  Scrolling
+//
+//  Created by Juan Hernandez Pazos on 11/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
